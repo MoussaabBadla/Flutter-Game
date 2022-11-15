b779cc95faf22229d765ef409b1d847a5616082d
MoussaabBadla <moussaab.badla@etu.usthb.dz>
2022-11-16 00:16:02 +0100

    build env

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,229 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  final game = DinoGame();
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          children: [
+            GameWidget(
+              game: game,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Align(
+              alignment: Alignment.bottomRight,
+              child: NavigationKeys(
+                onDirectionChanged: game.onArrowKeyChanged,
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ),
+  );
+}
+
+class DinoPlayer extends SpriteComponent with HasGameRef {
+  DinoPlayer() : super(size: Vector2.all(100.0));
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    sprite = await gameRef.loadSprite('Bunny/Purple.png');
+    position = gameRef.size / 2;
+  }
+
+  Direction direction = Direction.none;
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    updatePosition(dt);
+  }
+
+  updatePosition(double dt) {
+    switch (direction) {
+      case Direction.up:
+        position.y--;
+        break;
+      case Direction.down:
+        position.y++;
+        break;
+      case Direction.left:
+        position.x--;
+        break;
+      case Direction.right:
+        position.x++;
+        break;
+      case Direction.none:
+        break;
+    }
+  }
+}
+
+class DinoWorld extends SpriteComponent with HasGameRef {
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    sprite = await gameRef.loadSprite('Environment.png');
+    size = sprite!.originalSize;
+  }
+}
+
+class DinoGame extends FlameGame {
+  onArrowKeyChanged(Direction direction) {
+    _dinoPlayer.direction = direction;
+  }
+
+  DinoPlayer _dinoPlayer = DinoPlayer();
+  DinoWorld _dinoWorld = DinoWorld();
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    await add(_dinoWorld);
+    await add(_dinoPlayer);
+    _dinoPlayer.position = _dinoWorld.size / 1.5;
+    camera.followComponent(_dinoPlayer,
+        worldBounds: Rect.fromLTRB(0, 0, _dinoWorld.size.x, _dinoWorld.size.y));
+  }
+}
+
+class NavigationKeys extends StatefulWidget {
+  final ValueChanged<Direction>? onDirectionChanged;
+
+  const NavigationKeys({Key? key, required this.onDirectionChanged})
+      : super(key: key);
+
+  @override
+  State<NavigationKeys> createState() => _NavigationKeysState();
+}
+
+class _NavigationKeysState extends State<NavigationKeys> {
+  Direction direction = Direction.none;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 250,
+      width: 170,
+      child: Column(
+        children: [
+          ArrowKey(
+            icons: Icons.keyboard_arrow_up,
+            onTapDown: (det) {
+              updateDirection(Direction.up);
+            },
+            onTapUp: (dets) {
+              updateDirection(Direction.none);
+            },
+            onLongPressDown: () {
+              updateDirection(Direction.up);
+            },
+            onLongPressEnd: (dets) {
+              updateDirection(Direction.none);
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ArrowKey(
+                icons: Icons.keyboard_arrow_left,
+                onTapDown: (det) {
+                  updateDirection(Direction.left);
+                },
+                onTapUp: (dets) {
+                  updateDirection(Direction.none);
+                },
+                onLongPressDown: () {
+                  updateDirection(Direction.left);
+                },
+                onLongPressEnd: (dets) {
+                  updateDirection(Direction.none);
+                },
+              ),
+              ArrowKey(
+                icons: Icons.keyboard_arrow_right,
+                onTapDown: (det) {
+                  updateDirection(Direction.right);
+                },
+                onTapUp: (dets) {
+                  updateDirection(Direction.none);
+                },
+                onLongPressDown: () {
+                  updateDirection(Direction.right);
+                },
+                onLongPressEnd: (dets) {
+                  updateDirection(Direction.none);
+                },
+              ),
+            ],
+          ),
+          ArrowKey(
+            icons: Icons.keyboard_arrow_down,
+            onTapDown: (det) {
+              updateDirection(Direction.down);
+            },
+            onTapUp: (dets) {
+              updateDirection(Direction.none);
+            },
+            onLongPressDown: () {
+              updateDirection(Direction.down);
+            },
+            onLongPressEnd: (dets) {
+              updateDirection(Direction.none);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void updateDirection(Direction newDirection) {
+    direction = newDirection;
+    widget.onDirectionChanged!(direction);
+  }
+}
+
+class ArrowKey extends StatelessWidget {
+  const ArrowKey({
+    Key? key,
+    required this.icons,
+    required this.onTapDown,
+    required this.onTapUp,
+    required this.onLongPressDown,
+    required this.onLongPressEnd,
+  }) : super(key: key);
+  final IconData icons;
+  final Function(TapDownDetails) onTapDown;
+  final Function(TapUpDetails) onTapUp;
+  final Function() onLongPressDown;
+  final Function(LongPressEndDetails) onLongPressEnd;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: onTapDown,
+      onTapUp: onTapUp,
+      onLongPress: onLongPressDown,
+      onLongPressEnd: onLongPressEnd,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: const Color(0x88ffffff),
+          borderRadius: BorderRadius.circular(60),
+        ),
+        child: Icon(
+          icons,
+          size: 42,
+        ),
+      ),
     );
   }
 }
+
+enum Direction { up, down, left, right, none }
